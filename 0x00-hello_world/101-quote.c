36eccb8cf3c3afa85c2d0b64e03c0cbc7e893b04
nonsoflex <emmflexcsc400@gmail.com>
2023-07-13 08:23:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - main functions
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
