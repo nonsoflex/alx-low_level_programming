fbf7f95d7248486fc2cbd90ddeee5398ff66355b
nonsoflex <emmflexcsc400@gmail.com>
2023-07-14 14:19:29 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar(x + '0');
 				putchar(y + '0');
 				putchar(z + '0');
-				if (x != 7 && y != 8 && z != 9)
+				if (x != 7 || y != 8 || z != 9)
 				{
 					putchar(',');
 					putchar(' ');
