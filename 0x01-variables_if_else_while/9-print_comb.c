d46d2133ed677e5af5400a47da6dfbd163de44f8
nonsoflex <emmflexcsc400@gmail.com>
2023-07-14 11:33:09 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,15 @@ int main(void)
 {
 	int x;
 
-	for (x = 0 ; x < 10 ; x++)
+	for (x = 0 ; x < 9 ; x++)
 	{
 		putchar(x + '0');
-		putchar(',');
-		putchar(' ');
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
