cbfdd05fe0d5fb8624affe93786cb4ea34ed8aca
nonsoflex <emmflexcsc400@gmail.com>
2023-07-14 08:46:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		if (x != 'q' && x != 'e')
+			putchar(x);
+	putchar('\n');
+	return (0);
+}
