f951f2cbb2ba2f89335f56866698a0445823ff49
nonsoflex <emmflexcsc400@gmail.com>
2023-07-14 09:20:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main functions
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		printf("%d", 1);
+	printf("\n");
+	return (0);
+}
