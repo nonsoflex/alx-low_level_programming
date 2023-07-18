8c872c116864fa537fc80f7bc722714b98a6f7aa
nonsoflex <emmflexcsc400@gmail.com>
2023-07-18 05:27:14 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - this is the main functions _putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('a');
+_putchar('o');
+_putchar('v');
+_putchar('b');
+_putchar('\n');
+return (0);
+}
