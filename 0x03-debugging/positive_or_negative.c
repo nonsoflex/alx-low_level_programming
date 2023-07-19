8dd13d3776007d54821e5c597488a4e185d5280d
nonsoflex <emmflexcsc400@gmail.com>
2023-07-19 14:39:21 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/**
+ * positive_or_negative - functions of i
+ * @i: void
+ */
+
 void positive_or_negative(int i)
 {
 if (i > 0)
 printf("%d is positive", i);
-else if (i == 0)
-printf("%d is zero", i);
-else
+else if (i < 0)
 printf("%d is negative", i);
+else
+printf("%d is zero", i);
 printf("\n");
 }
