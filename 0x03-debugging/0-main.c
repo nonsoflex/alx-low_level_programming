6dd18bb678c7fa6591db630647a74e4c4278624b
nonsoflex <emmflexcsc400@gmail.com>
2023-07-20 14:03:14 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: always 0
-*/
+ * main - tests function that prints integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -10,6 +11,6 @@ int i;
 
 i = 0;
 positive_or_negative(i);
-
+	
 return (0);
 }
