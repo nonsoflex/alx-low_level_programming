2605403e925810cce6105fe9fa38a14245b91784
nonsoflex <emmflexcsc400@gmail.com>
2023-07-24 05:53:04 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int -  a function that swaps the values of two integers
+ *
+ * @a: write parameter
+ *
+ * @b: write paramter
+ *
+ * Return: 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
