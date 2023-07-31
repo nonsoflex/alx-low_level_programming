55fe2ef186b39d67e1cf18b419a2887703a10361
nonsoflex <emmflexcsc400@gmail.com>
2023-07-31 02:52:46 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* set_string - sets the value of a pointer to a char
+* @s: double pointer to set
+* @to: the char to point to
+*
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
