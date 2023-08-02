4ca8d9a79eb9ee2abcdc984191567c0a04b472ac
nonsoflex <emmflexcsc400@gmail.com>
2023-08-02 02:25:34 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints a string with recursion
+*
+* @s: string input params
+*
+* Return: no return
+*/
+
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
