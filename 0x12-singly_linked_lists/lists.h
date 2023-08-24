132c94750924e427882865bc007a306d1ee9999f
nonsoflex <emmflexcsc400@gmail.com>
2023-08-24 05:21:56 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,21 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: All prototypes used in the singly linked directory
- * are contained here and is part of the necessities for succesfull
- * program compilation.
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
