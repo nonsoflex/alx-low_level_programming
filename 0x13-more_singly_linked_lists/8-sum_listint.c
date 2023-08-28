3316578bcf397fdabad33524735c101fedd5d721
nonsoflex <emmflexcsc400@gmail.com>
2023-08-28 15:38:47 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* sum_listint - function that sums all elements of a list
+*
+* @head: point to the head of the list
+*
+* Return: sum of all elements
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sums;
+
+	for (sums = 0; head; sums += head->n, head = head->next)
+;
+	return (sums);
+}
