21048ddc75ad0787ce1eb62f07ee59536e557a51
nonsoflex <emmflexcsc400@gmail.com>
2023-08-28 03:07:00 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of as listint_t list
+ *
+ * @h: Pointer to the head node of the list
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
